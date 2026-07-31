a63c4609a715c645a6518d76fcc97cd8d8c281df
vschw <vschwitzky@t-online.de>
2026-07-31 14:53:10 +0200

    move split_vote to shared_utils

--- simulate_or_proof.py
+++ simulate_or_proof.py
@@ -1,15 +1,11 @@
-import secrets
-
 from commitment import commitment
 from or_proof import prover, verifier
-from config.params import N_A, Q
+from utils.shared_utils import split_vote
+
 
 # --------------------------------------------------------
-#  Simulation of an OR-proof
+#  Simulation of an OR-proof/Voter
 # --------------------------------------------------------
-
-SECURE_RNG = secrets.SystemRandom()  # instantiate a cryptographically secure random number generator
-
 
 def simulate_or_proof(m):
     """
@@ -67,32 +63,3 @@ def simulate_voter(C, m):
     # out of scope for this implementation
 
     return r0, r1, f0, f1, summed_commitments
-
-
-# --------------------------------------------------------
-#  Helper Functions
-# --------------------------------------------------------
-
-def split_vote(v_i):
-    """
-    Splits a vote v_i (should 0 or 1) into N_A additive shares modulo Q.
-
-    Args:
-        v_i: The vote
-
-    Returns:
-        A list of N_A integers, each in [0, Q-1].
-        The sum of these shares modulo Q equals v_i.
-    """
-    shares = []
-
-    # Generate the first N_A - 1 shares uniformly at random modulo Q
-    for _ in range(N_A - 1):
-        share = SECURE_RNG.randrange(Q)  # picks an integer from 0 to Q-1 inclusive
-        shares.append(share)
-
-    # Compute the final share so that total_sum ≡ v_i (mod Q)
-    last_share = (v_i - sum(shares)) % Q
-    shares.append(last_share)
-
-    return shares
--- utils/shared_utils.py
+++ utils/shared_utils.py
@@ -1,12 +1,14 @@
 import struct
 from math import sqrt
+import secrets
 
-from config.params import N
-
+from config.params import N, Q, N_A
 
 # --------------------------------------------------------
 #  Helper functions for the Commitment, Prover and Verifier
 # --------------------------------------------------------
+
+SECURE_RNG = secrets.SystemRandom()  # instantiate a cryptographically secure random number generator
 
 
 def norm_rq_vector(vec):
@@ -36,3 +38,28 @@ def serialize_rq_vector(vec):
     for poly in vec:
         parts.append(struct.pack('<' + 'i' * N, *[int(c.lift_centered()) for c in poly.list()]))
     return b''.join(parts)
+
+
+def split_vote(v_i):
+    """
+    Splits a vote v_i (should 0 or 1) into N_A additive shares modulo Q.
+
+    Args:
+        v_i: The vote
+
+    Returns:
+        A list of N_A integers, each in [0, Q-1].
+        The sum of these shares modulo Q equals v_i.
+    """
+    shares = []
+
+    # Generate the first N_A - 1 shares uniformly at random modulo Q
+    for _ in range(N_A - 1):
+        share = SECURE_RNG.randrange(Q)  # picks an integer from 0 to Q-1 inclusive
+        shares.append(share)
+
+    # Compute the final share so that total_sum ≡ v_i (mod Q)
+    last_share = (v_i - sum(shares)) % Q
+    shares.append(last_share)
+
+    return shares
